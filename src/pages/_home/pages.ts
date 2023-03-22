d653fd4a88590a21a9018721842c7485a2380f79
Zachary Sakowitz <zsakowitz@gmail.com>
2023-03-22 07:24:38 -0700

    Fix issues

--- src/pages/_home/pages.ts
+++ src/pages/_home/pages.ts
@@ -1,22 +1,20 @@
 import { getCollection } from "astro:content"
+import debugMathToGLSLOpenGraph from "../debug/math-to-glsl/open-graph.png"
+import deduplicateTextOpenGraph from "../deduplicate-text/open-graph.jpg"
+import fractalExplorerOpenGraph from "../fractal-explorer/open-graph.png"
 
-export const allTags = [
-  "art",
-  "blog",
-  "code",
-  "debug",
-  "language",
-  "math",
-  "meta",
-  "stories",
-  "tool",
-] as const
+export type Tag =
+  | "art"
+  | "blog"
+  | "code"
+  | "debug"
+  | "language"
+  | "math"
+  | "meta"
+  | "stories"
+  | "tool"
 
-export type Tag = (typeof allTags)[number]
-
-export const allCategories = ["blog", "debug"] as const
-
-export type Category = (typeof allCategories)[number]
+export type Category = "blog" | "debug"
 
 export interface Page {
   title: string
@@ -32,6 +30,51 @@ export interface Page {
   imageSrc: string
   imageAlt: string
 }
+
+export const nonBlogPages: readonly Page[] = [
+  {
+    title: "Fractal Explorer",
+    href: "/fractal-explorer",
+    shortSubtitle: "Explore fractals with custom equations and color palettes.",
+    longSubtitle:
+      "Explore beautiful mathematical fractals by using custom equations, zooming deeply into nested structures, and adjusting many settings to vary the color scheme.",
+
+    tags: ["art", "math"],
+
+    imageAlt: "A multicolored zoom of the Mandelbrot Set.",
+    imageSrc: fractalExplorerOpenGraph,
+  },
+
+  {
+    title: "Deduplicate Text",
+    href: "/deduplicate-text",
+    shortSubtitle:
+      "A tool to remove repeated letters (e.g. Hello world => He wrld).",
+    longSubtitle:
+      "A tool that removes repeated letters in text. For example, 'Hello world' is converted to 'He wrld.'",
+
+    tags: ["tool"],
+
+    imageAlt:
+      "Two rows of text. The first reads 'Deduplicate Text', with arrows pointing the first two Ds, Es, and Ts to each other. The second row reads 'uplica ext,' with the Ds, Es, and Ts in the first row removed.",
+    imageSrc: deduplicateTextOpenGraph,
+  },
+
+  {
+    title: "Debug: Math to GLSL",
+    href: "/debug/math-to-glsl",
+    shortSubtitle: "A tool which converts math expressions to GLSL.",
+    longSubtitle:
+      "A tool for debugging the math to GLSL converter used internally by the Fractal Explorer and other pages featuring customizable mathematical content.",
+
+    tags: ["debug", "math"],
+    category: "debug",
+
+    imageAlt:
+      "A light grey bug icon to the left of text saying 'Debug Page: Math to GLSL.' Yellow and black construction tape surrounds the other elements.",
+    imageSrc: debugMathToGLSLOpenGraph,
+  },
+]
 
 export const blogPages: readonly Page[] = await Promise.all(
   (
@@ -68,49 +111,4 @@ export const blogPages: readonly Page[] = await Promise.all(
     })),
 )
 
-export const pages: readonly Page[] = [
-  ...blogPages,
-
-  {
-    title: "Fractal Explorer",
-    href: "/fractal-explorer",
-    shortSubtitle: "Explore fractals with custom equations and color palettes.",
-    longSubtitle:
-      "Explore beautiful mathematical fractals by using custom equations, zooming deeply into nested structures, and adjusting many settings to vary the color scheme.",
-
-    tags: ["art", "math"],
-
-    imageAlt: "A multicolored zoom of the Mandelbrot Set.",
-    imageSrc: (await import("../fractal-explorer/open-graph.png")).default,
-  },
-
-  {
-    title: "Deduplicate Text",
-    href: "/deduplicate-text",
-    shortSubtitle:
-      "A tool to remove repeated letters (e.g. Hello world => He wrld).",
-    longSubtitle:
-      "A tool that removes repeated letters in text. For example, 'Hello world' is converted to 'He wrld.'",
-
-    tags: ["tool"],
-
-    imageAlt:
-      "Two rows of text. The first reads 'Deduplicate Text', with arrows pointing the first two Ds, Es, and Ts to each other. The second row reads 'uplica ext,' with the Ds, Es, and Ts in the first row removed.",
-    imageSrc: (await import("../deduplicate-text/open-graph.jpg")).default,
-  },
-
-  {
-    title: "Debug: Math to GLSL",
-    href: "/debug/math-to-glsl",
-    shortSubtitle: "A tool which converts math expressions to GLSL.",
-    longSubtitle:
-      "A tool for debugging the math to GLSL converter used internally by the Fractal Explorer and other pages featuring customizable mathematical content.",
-
-    tags: ["debug", "math"],
-    category: "debug",
-
-    imageAlt:
-      "A light grey bug icon to the left of text saying 'Debug Page: Math to GLSL.' Yellow and black construction tape surrounds the other elements.",
-    imageSrc: (await import("../debug/math-to-glsl/open-graph.png")).default,
-  },
-]
+export const pages: readonly Page[] = [...blogPages, ...nonBlogPages]
