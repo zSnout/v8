a0bfca980614034795a2ffcb4dc2419df69cccd8
Zachary Sakowitz <zsakowitz@gmail.com>
2023-03-22 12:42:57 -0700

    Fix cards

--- src/pages/_home/cards/BlogCard.tsx
+++ src/pages/_home/cards/BlogCard.tsx
@@ -6,9 +6,14 @@ export function BlogCard(props: { class: string; page: Page }) {
     <div class={"w-full flex-col " + props.class}>
       <BlueGreenText name={props.page.tags[0]} />
 
-      <p class="mt-1 text-xl font-bold text-z-heading">{props.page.title}</p>
+      <a
+        class="mt-1 text-xl font-bold text-z-heading underline decoration-transparent transition hover:text-z-link hover:decoration-z-text-link"
+        href={props.page.href}
+      >
+        {props.page.title}
+      </a>
 
-      <p class="mt-2 text-sm text-z-subtitle [line-height:1.5]">
+      <p class="mt-2 text-sm text-z-subtitle transition [line-height:1.5]">
         {props.page.longSubtitle}
       </p>
     </div>
--- src/pages/_home/cards/CompactCard.tsx
+++ src/pages/_home/cards/CompactCard.tsx
@@ -1,9 +1,23 @@
+import { Show } from "solid-js"
+import { BlueGreenText } from "../BlueGreenText"
 import type { Page } from "../pages"
 
 export function CompactCard(props: { page: Page }) {
   return (
     <div class="flex flex-col">
-      <p class="text-xl font-bold text-z-heading">{props.page.title}</p>
+      <Show when={props.page.category}>
+        <BlueGreenText name={props.page.category || ""} />
+      </Show>
+
+      <a
+        class="text-xl font-bold text-z-heading underline decoration-transparent transition hover:text-z-link hover:decoration-z-text-link"
+        classList={{
+          "mt-1": props.page.category != null,
+        }}
+        href={props.page.href}
+      >
+        {props.page.title}
+      </a>
 
       <p class="mt-2 text-sm text-z-subtitle [line-height:1.5]">
         {props.page.longSubtitle}
