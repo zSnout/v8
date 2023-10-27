c00285b9bad85995165456f1f20a91ace7276115
Zachary <zsakowitz@gmail.com>
2023-10-27 13:43:31 -0700

    Update things

--- src/pages/launchpad/index.tsx
+++ src/pages/launchpad/index.tsx
@@ -1,7 +1,7 @@
 import openGraphV8 from "../_home/open-graph.jpg"
 import openGraphV7 from "@/assets/zsnout-7.png"
 import openGraphV6 from "./v6-open-graph.jpg"
-import { createSignal, onMount } from "solid-js"
+import { createSignal } from "solid-js"
 
 interface Page {
   readonly title: string
@@ -13,20 +13,19 @@ interface Page {
 const pages: readonly Page[] = [
   {
     title: "zSnout 8",
-    description:
-      "The latest version of zSnout, with the new blog and Ithkuil projects.",
+    description: "The latest version of zSnout.",
     href: "https://v8.zsnout.com/",
     image: openGraphV8,
   },
   {
     title: "zSnout 7",
-    description: "",
+    description: "The previous version of zSnout.",
     href: "https://zsnout.com/home",
     image: openGraphV7,
   },
   {
     title: "zSnout 6",
-    description: "",
+    description: "A very old version of zSnout.",
     href: "https://v6.zsnout.com/",
     image: openGraphV6,
   },
@@ -43,7 +42,7 @@ export function Main() {
   return pages.map((page) => {
     return (
       <a
-        class="group relative flex aspect-open-graph"
+        class="group relative flex aspect-open-graph focus-visible:outline-none"
         href={page.href}
         onMouseEnter={(event) => {
           const animated = event.currentTarget.children[0] as HTMLDivElement
@@ -71,19 +70,9 @@ export function Main() {
           const animated = event.currentTarget.children[0] as HTMLDivElement
 
           animated.style.transitionDuration = "150ms"
-
-          // animated.animate(
-          //   [
-          //     {
-          //       offset: 0,
-          //       transform: "perspective(200px) rotateX(0deg) rotateY(0deg)",
-          //     },
-          //   ],
-          //   { duration: 500, composite: "accumulate" },
-          // )
         }}
       >
-        <div class="pointer-events-none relative flex h-full w-full flex-col overflow-hidden rounded-lg px-4 py-3 transition duration-0 group-hover:z-20 [&:not(.group:hover_*)]:![transform:scale(100%)_perspective(200px)_rotateX(0deg)_rotateY(0deg)]">
+        <div class="pointer-events-none relative flex h-full w-full flex-col overflow-hidden rounded-lg border border-transparent px-4 py-3 ring-z-focus transition duration-0 group-hover:z-20 group-focus-visible:border-z-focus group-focus-visible:ring [&:not(.group:hover_*)]:![transform:scale(100%)_perspective(200px)_rotateX(0deg)_rotateY(0deg)]">
           <img
             class="absolute left-0 top-0 h-full w-full select-none"
             src={page.image}
@@ -91,28 +80,12 @@ export function Main() {
 
           <div class="absolute left-0 top-0 h-full w-full bg-gradient-to-b from-[#0002] to-current text-[#0008] transition group-hover:to-90% group-hover:text-[#000c]" />
 
-          <div class="relative mt-auto flex translate-y-[calc(var(--description-height)_+_0.25rem)] flex-col transition [--description-height:0.75rem] group-hover:![--description-height:-0.25rem]">
+          <div class="relative mt-auto flex flex-col transition [transform:translate3d(0,0,200px)]">
             <p class="text-2xl font-semibold text-white drop-shadow">
               {page.title}
             </p>
 
-            <p
-              class="mt-1 text-xs text-white opacity-0 drop-shadow-sm group-hover:opacity-100"
-              ref={(self) => {
-                const parent = self.parentElement as HTMLDivElement
-
-                function onResize() {
-                  parent.style.setProperty(
-                    "--description-height",
-                    self.getBoundingClientRect().height + "px",
-                  )
-                }
-
-                onMount(onResize)
-                addEventListener("resize", onResize)
-                self.addEventListener("resize", onResize)
-              }}
-            >
+            <p class="mt-1 text-xs text-white drop-shadow-sm">
               {page.description}
             </p>
           </div>
