b0a1b1d40809386adb40a383d2d4e7fb1582dfda
Zachary <zsakowitz@gmail.com>
2024-01-03 19:22:50 +0100

    Make blog background work

---
+++ src/components/Prose.tsx
@@ -0,0 +1,29 @@
+export function Unprose(props: { children?: any; class?: string }) {
+  return (
+    <div
+      class={
+        "relative left-[calc(-50vw_+_min(50vw_-_1.5rem,32.5ch))] w-[100vw] print:mx-0" +
+        (props.class ? " " + props.class : "")
+      }
+    >
+      {props.children}
+    </div>
+  )
+}
+
+export function Reprose(props: { children?: any; class?: string }) {
+  return (
+    <div class="mx-6 flex flex-col print:mx-0">
+      <div class="flex w-full gap-12 text-z transition">
+        <div
+          class={
+            "mx-auto w-full max-w-prose" +
+            (props.class ? " " + props.class : "")
+          }
+        >
+          {props.children}
+        </div>
+      </div>
+    </div>
+  )
+}
