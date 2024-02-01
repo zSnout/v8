7e8faea541abf30b0c6581c94866322fd4109622
Zachary <zsakowitz@gmail.com>
2024-01-31 20:24:32 -0800

    Add things

--- src/pages/ithkuil/script/canvas/index.tsx
+++ src/pages/ithkuil/script/canvas/index.tsx
@@ -7,75 +7,25 @@ export function path(data: TemplateStringsArray, ...values: number[]): string {
   )
 }
 
-export function scriptify1(
-  [x1, y1]: [x: number, y: number],
-  [x2, y2]: [x: number, y: number],
-): string {
-  // if (y1 < y2) {
-  //   ;[x1, y1, x2, y2] = [x2, y2, x1, y1]
-  // }
+export type Offset =
+  | { readonly h: number; readonly v?: undefined; readonly d?: number }
+  | { readonly v: number; readonly h?: undefined }
+  | { readonly x: number; readonly y: number; readonly d?: number }
 
-  const angle = Math.atan2(y2 - y1, x2 - x1) - Math.PI / 2
-  const tx = 10 * Math.cos(angle)
-  const ty = 10 * Math.sin(angle)
-  const x4 =
-    ((y1 - y2) * (x1 + tx) + (x2 - x1) * (ty - x1)) / (x1 + y1 - x2 - y2)
-  const y4 = x1 + y1 - x4
-  const x3 = x4 - x1 + x2
-  const y3 = y4 - y1 + y2
-
-  return path`M ${x1} ${y1} L ${x2} ${y2} L ${x3} ${y3} L ${x4} ${y4} Z`
+export type Path = {
+  readonly x: number
+  readonly y: number
+  readonly path: readonly Offset[]
 }
-
-export function scriptify(
-  [x1, y1]: [x: number, y: number],
-  [x2, y2]: [x: number, y: number],
-): string {
-  const angle = (Math.atan2(y2 - y1, x2 - x1) + 2 * Math.PI) % Math.PI
-
-  const [tx, ty] = angle == 0 || angle == Math.PI ? [-10, 10] : [7.5, -7.5]
-
-  const x4 = x1 + tx
-  const y4 = y1 + ty
-  const x3 = x2 + tx
-  const y3 = y2 + ty
-
-  return path`M ${x1} ${y1} L ${x2} ${y2} L ${x3} ${y3} L ${x4} ${y4} Z`
-}
-
-export function all(points: [number, number][]) {
-  return (
-    <>
-      {points.map(([x, y]) => (
-        <path
-          d={`M ${x} ${y} l 0.01 0`}
-          stroke="red"
-          stroke-width={5}
-          stroke-linecap="round"
-          stroke-linejoin="round"
-        />
-      ))}
-
-      {points.slice(1).map((p2, i) => (
-        <path d={scriptify(points[i]!, p2)} />
-      ))}
-    </>
-  )
-}
-
-type Offset =
-  | { h: number; v?: undefined; d?: number }
-  | { h?: undefined; v: number }
-  | { x: number; y: number; d?: number }
-
-const THIN_STROKE_WIDTH = 2.5
-const THIN_STROKE_DIAG_WIDTH = 2.5 * Math.SQRT1_2
 
 export function generatePaths(
   x: number,
   y: number,
-  offsets: Offset[],
+  offsets: readonly Offset[],
 ): string[] {
+  const THIN_STROKE_WIDTH = 2.5
+  const THIN_STROKE_DIAG_WIDTH = 2.5 * Math.SQRT1_2
+
   const outputs: string[] = []
 
   for (let index = 0; index < offsets.length; index++) {
@@ -90,22 +40,27 @@ h ${offset.h}
 l ${-offset.d - 5} ${offset.d + 5}
 h ${-THIN_STROKE_WIDTH}
 l ${offset.d - 5} ${-offset.d + 5}
-h ${-offset.h + THIN_STROKE_WIDTH}
-z`,
+h ${-offset.h + THIN_STROKE_WIDTH}`,
         )
 
         x += offset.h - offset.d
         y += offset.d - THIN_STROKE_WIDTH
       } else {
         outputs.push(
-          path`M ${x + 5} ${y - 5} h ${offset.h} l -10 10 h ${-offset.h} z`,
+          path`M ${x + 5} ${y - 5}
+h ${offset.h}
+l -10 10
+h ${-offset.h}`,
         )
 
         x += offset.h
       }
     } else if ("v" in offset && offset.v != null) {
       outputs.push(
-        path`M ${x + 5} ${y - 5} v ${offset.v} l -10 10 v ${-offset.v} z`,
+        path`M ${x + 5} ${y - 5}
+v ${offset.v}
+l -10 10
+v ${-offset.v}`,
       )
 
       y += offset.v
@@ -121,17 +76,17 @@ l ${THIN_STROKE_DIAG_WIDTH * Math.cos(angle)} ${
           THIN_STROKE_DIAG_WIDTH * Math.sin(angle)
         }
 l ${offset.d - 3.75} ${-offset.d + 3.75}
-L ${x - 3.75} ${y + 3.75}
-z`,
+L ${x - 3.75} ${y + 3.75}`,
       )
 
       x += offset.x - offset.d
       y += offset.y + offset.d - THIN_STROKE_WIDTH
     } else {
       outputs.push(
-        path`M ${x + 3.75} ${y - 3.75} l ${offset.x} ${
-          offset.y
-        } l -7.5 7.5 l ${-offset.x} ${-offset.y} z`,
+        path`M ${x + 3.75} ${y - 3.75}
+l ${offset.x} ${offset.y}
+l -7.5 7.5
+l ${-offset.x} ${-offset.y}`,
       )
 
       x += offset.x
@@ -142,21 +97,48 @@ z`,
   return outputs
 }
 
+const CORE_PATHS = {
+  r: {
+    x: 3.75,
+    y: 3.75,
+    path: [{ x: 32.5, y: 32.5, d: 31.25 }, { h: 40 }],
+  },
+
+  l: {
+    x: 10,
+    y: 5,
+    path: [{ v: 35 }, { h: 22.4, d: 30 }],
+  },
+} satisfies Record<string, Path> as Record<string, Path>
+
 export function Main() {
   return (
     <svg class="m-auto w-full overflow-visible" viewBox="-20 -20 100 100">
       <g>
-        <path d="M 0 0 l 40 0 l 0 70 l -40 0 z" fill="red" />
-        {generatePaths(3.75, 3.75, [
-          { x: 32.5, y: 32.5, d: 31.25 },
-          { h: 40 },
-        ]).map((x) => (
+        <path d="M 0 0      H 40      V 70 H 0 z" fill="red" />
+
+        {generatePaths(
+          Object.values(CORE_PATHS).at(-1)!.x,
+          Object.values(CORE_PATHS).at(-1)!.y,
+          Object.values(CORE_PATHS).at(-1)!.path,
+        ).map((x) => (
           <path d={x} />
         ))}
-        <path d="M 0 0 l 40 0 l 0 70 l -40 0 z" stroke="green" fill="none" />
-        <Blink>
-          <path transform="translate(25 35)" d={CORES.r.shape} fill="blue" />
-        </Blink>
+
+        <Blink
+          children={
+            (
+              <path
+                transform="translate(25 35)"
+                d={
+                  CORES[Object.keys(CORE_PATHS).at(-1) as keyof typeof CORES]
+                    ?.shape
+                }
+                fill="blue"
+              />
+            ) as SVGPathElement
+          }
+        />
       </g>
     </svg>
   )
