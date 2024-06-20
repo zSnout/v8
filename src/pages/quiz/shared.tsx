8a5446faad5dcf25afb5fc97cc7d795dcafa0cf4
Zachary <zsakowitz@gmail.com>
2024-06-20 09:28:33 -0700

    move tree to separate file

---
+++ src/pages/quiz/shared.tsx
@@ -0,0 +1,68 @@
+import type { JSX } from "solid-js"
+
+export type Leaf = DirectTreeCard | Generator
+
+export class PartialCard {
+  static of(base: Leaf, id: string | undefined): PartialCard {
+    if (base instanceof DirectTreeCard) {
+      return base.toPartial()
+    } else if (base instanceof Generator) {
+      return base.generate(id)
+    } else {
+      throw new TypeError("Invalid card generator.")
+    }
+  }
+
+  private declare __brand
+
+  constructor(
+    readonly short: string,
+    readonly front: JSX.Element,
+    readonly back: JSX.Element,
+    readonly source: readonly string[],
+    readonly id: string,
+  ) {}
+
+  toCard(path: readonly string[], lastInterval: number | undefined): Card {
+    return { ...this, path: path, answerShown: false, lastInterval }
+  }
+}
+
+export class DirectTreeCard {
+  private declare __brand2
+
+  constructor(
+    readonly short: string,
+    readonly front: JSX.Element,
+    readonly back: JSX.Element,
+    readonly source: readonly string[],
+    readonly weight: number,
+  ) {}
+
+  toPartial(): PartialCard {
+    return new PartialCard(this.short, this.front, this.back, this.source, "")
+  }
+}
+
+export class Generator {
+  private declare __brand3
+  readonly weight: number
+
+  constructor(
+    readonly generate: (id?: string | undefined) => PartialCard,
+    readonly cardCount: number,
+  ) {
+    this.weight = 1 / cardCount
+  }
+}
+
+export interface Card {
+  readonly front: JSX.Element
+  readonly back: JSX.Element
+  readonly path: readonly string[]
+  readonly id: string
+  readonly answerShown: boolean
+  readonly source: readonly string[]
+  readonly short: string
+  readonly lastInterval: number | undefined
+}
