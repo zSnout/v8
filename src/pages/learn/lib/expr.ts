a5ced8d62b0d49e67d834a63812efbcfbcddf6af
Zachary <zsakowitz@gmail.com>
2024-06-27 17:17:34 -0700

    stop solid warnings

--- src/pages/learn/lib/expr.ts
+++ src/pages/learn/lib/expr.ts
@@ -1,9 +1,8 @@
-import { Accessor, createEffect, createSignal } from "solid-js"
+import { Accessor, createSignal } from "solid-js"
 
 export function createExpr<T>(
   getValue: (last?: T) => T,
 ): [get: Accessor<T>, reload: () => T] {
-  const [get, set] = createSignal<T>()
-  createEffect(() => set(getValue))
+  const [get, set] = createSignal<T>(getValue())
   return [get as Accessor<T>, () => set(getValue)]
 }
