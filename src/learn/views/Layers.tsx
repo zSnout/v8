8111b3f1e5ca966a2148ffb7d0aea290ffce5dd1
Zachary <zsakowitz@gmail.com>
2024-07-03 18:05:42 -0700

    move inert down in case `Layers~push` fails

--- src/learn/views/Layers.tsx
+++ src/learn/views/Layers.tsx
@@ -20,20 +20,7 @@ export function useLayers() {
 export class Layers {
   static Root(props: { children: JSX.Element }) {
     const layers = new Layers(getOwner())
-    // onMount(() => {
-    //   addEventListener("keydown", (event) => {
-    //     if (
-    //       !event.altKey &&
-    //       !event.ctrlKey &&
-    //       !event.metaKey &&
-    //       event.key == "Escape" &&
-    //       layers.layers.length >= 1
-    //     ) {
-    //       event.preventDefault()
-    //       layers.popLatest()
-    //     }
-    //   })
-    // })
+
     return (
       <LayerContext.Provider value={layers}>
         <div class="relative min-h-full w-full">
@@ -63,8 +50,6 @@ export class Layers {
     }
 
     const previouslyFocused = document.activeElement
-    console.log(previouslyFocused)
-    prev.inert = true
 
     const child = runWithOwner(this.owner, () => el(pop))
     const next = (
@@ -81,6 +66,7 @@ export class Layers {
       </div>
     ) as HTMLDivElement
 
+    prev.inert = true
     prev.after(next)
     return pop
   }
