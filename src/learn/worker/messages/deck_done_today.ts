5f8d3357b499ebc7d74ec6490b9c68649675e988
Zachary <zsakowitz@gmail.com>
2024-07-18 21:49:31 -0700

    add some new queries

---
+++ src/learn/worker/messages/deck_done_today.ts
@@ -0,0 +1,34 @@
+import { isSameDaySync } from "@/learn/db/day"
+import type { Id } from "@/learn/lib/id"
+import { db } from "../db"
+
+export function deck_done_today(decks: Id[], dayStart: number) {
+  const stmt = db.prepare(
+    "SELECT today, new_today, revcards_today FROM decks WHERE id = ?",
+  )
+
+  let new_today = 0
+  let rev_today = 0
+
+  try {
+    for (const did of decks) {
+      stmt.bind([did])
+      const value = stmt.get() as [
+        today: number,
+        new_today: string,
+        revcards_today: string,
+      ]
+
+      const isToday = isSameDaySync(dayStart, Date.now(), value[0])
+
+      if (isToday) {
+        new_today += (JSON.parse(value[1]) as number[]).length
+        rev_today += (JSON.parse(value[2]) as number[]).length
+      }
+    }
+
+    return { new_today, rev_today }
+  } finally {
+    stmt.free()
+  }
+}
---
+++ src/learn/worker/messages/deck_limits.ts
@@ -0,0 +1,86 @@
+import { isSameDaySync } from "@/learn/db/day"
+import { type Id, ID_ZERO } from "@/learn/lib/id"
+import { id, int, qint } from "../checks"
+import { db } from "../db"
+import { prefs_get } from "./prefs_get"
+
+export function deck_limits_txless(main: Id | null, dayStart: number): Limits {
+  const [
+    cfid,
+    custom_revcard_limit,
+    custom_newcard_limit,
+    default_revcard_limit,
+    default_newcard_limit,
+    today,
+  ] =
+    main == null
+      ? [ID_ZERO, null, null, null, null]
+      : (db.checked(
+          "SELECT cfid, custom_revcard_limit, custom_newcard_limit, default_revcard_limit, default_newcard_limit, today FROM decks WHERE id = ?",
+          [id, qint, qint, qint, qint, int],
+          [main],
+        ).values[0] ?? [ID_ZERO, null, null, null, null])
+
+  const [new_per_day, rev_per_day] = db.checked(
+    "SELECT new_per_day, review_per_day FROM confs WHERE id = ?",
+    [int, int],
+    [cfid],
+  ).values[0]!
+
+  const isToday =
+    (today != null && isSameDaySync(dayStart, Date.now(), today)) || null
+
+  return {
+    new: {
+      deckToday: isToday && custom_newcard_limit,
+      deckDefault: default_newcard_limit,
+      confStandard: new_per_day,
+      now:
+        (isToday && custom_newcard_limit) ??
+        default_newcard_limit ??
+        new_per_day,
+    },
+    rev: {
+      deckToday: isToday && custom_revcard_limit,
+      deckDefault: default_revcard_limit,
+      confStandard: rev_per_day,
+      now:
+        (isToday && custom_revcard_limit) ??
+        default_revcard_limit ??
+        rev_per_day,
+    },
+  }
+}
+
+export interface Limit {
+  deckToday: number | null
+  deckDefault: number | null
+  confStandard: number
+  now: number
+}
+
+export interface Limits {
+  new: Limit
+  rev: Limit
+}
+
+export function deck_limits(main: Id | null): Limits {
+  const tx = db.tx()
+  try {
+    return deck_limits_txless(main, prefs_get().day_start)
+  } finally {
+    tx.dispose()
+  }
+}
+
+export interface Limit {
+  deckToday: number | null
+  deckDefault: number | null
+  confStandard: number
+  now: number
+}
+
+export interface Limits {
+  new: Limit
+  rev: Limit
+}
