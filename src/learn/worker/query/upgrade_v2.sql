8b47e7ae36879eaeb8e8fd0a30d2239126fabfe1
Zachary <zsakowitz@gmail.com>
2024-07-25 20:34:38 -0700

    hah alternative formatting more like the new parser doesn't support common syntax

--- src/learn/worker/query/upgrade_v2.sql
+++ src/learn/worker/query/upgrade_v2.sql
@@ -1,49 +1,61 @@
-DROP TABLE graves;
+DROP TABLE IF EXISTS graves;
 
-CREATE TABLE IF NOT EXISTS graves (
+CREATE TABLE graves (
   oid INTEGER NOT NULL,
   type INTEGER NOT NULL,
   UNIQUE (type, oid)
 );
 
-CREATE TRIGGER IF NOT EXISTS graves_from_cards AFTER DELETE ON cards BEGIN INSERT
-OR IGNORE INTO graves (oid, type)
-VALUES
-  (old.id, 0);
-
+CREATE TRIGGER IF NOT EXISTS graves_from_cards
+AFTER DELETE ON cards
+BEGIN
+  INSERT OR IGNORE INTO graves
+    (oid, type)
+  VALUES
+    (old.id, 0);
 END;
 
-CREATE TRIGGER IF NOT EXISTS graves_from_notes AFTER DELETE ON notes BEGIN INSERT
-OR IGNORE INTO graves (oid, type)
-VALUES
-  (old.id, 1);
-
+CREATE TRIGGER IF NOT EXISTS graves_from_notes
+AFTER DELETE ON notes
+BEGIN
+  INSERT OR IGNORE INTO graves
+    (oid, type)
+  VALUES
+    (old.id, 1);
 END;
 
-CREATE TRIGGER IF NOT EXISTS graves_from_decks AFTER DELETE ON decks BEGIN INSERT
-OR IGNORE INTO graves (oid, type)
-VALUES
-  (old.id, 2);
-
+CREATE TRIGGER IF NOT EXISTS graves_from_decks
+AFTER DELETE ON decks
+BEGIN
+  INSERT OR IGNORE INTO graves
+    (oid, type)
+  VALUES
+    (old.id, 2);
 END;
 
-CREATE TRIGGER IF NOT EXISTS graves_from_models AFTER DELETE ON models BEGIN INSERT
-OR IGNORE INTO graves (oid, type)
-VALUES
-  (old.id, 3);
-
+CREATE TRIGGER IF NOT EXISTS graves_from_models
+AFTER DELETE ON models
+BEGIN
+  INSERT OR IGNORE INTO graves
+    (oid, type)
+  VALUES
+    (old.id, 3);
 END;
 
-CREATE TRIGGER IF NOT EXISTS graves_from_prefs AFTER DELETE ON prefs BEGIN INSERT
-OR IGNORE INTO graves (oid, type)
-VALUES
-  (old.id, 4);
-
+CREATE TRIGGER IF NOT EXISTS graves_from_prefs
+AFTER DELETE ON prefs
+BEGIN
+  INSERT OR IGNORE INTO graves
+    (oid, type)
+  VALUES
+    (old.id, 4);
 END;
 
-CREATE TRIGGER IF NOT EXISTS graves_from_charts AFTER DELETE ON charts BEGIN INSERT
-OR IGNORE INTO graves (oid, type)
-VALUES
-  (old.id, 4);
-
+CREATE TRIGGER IF NOT EXISTS graves_from_charts
+AFTER DELETE ON charts
+BEGIN
+  INSERT OR IGNORE INTO graves
+    (oid, type)
+  VALUES
+    (old.id, 4);
 END;
